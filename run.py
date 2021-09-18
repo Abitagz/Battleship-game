1e8c240ed8bfa4a5395bede120ce0b14cc3dc127
Abitagz <helloabita2005@yahoo.com>
2021-09-18 21:37:46 +0000

    Add functions to my run.py

--- run.py
+++ run.py
@@ -1,1 +1,14 @@
 
+
+
+
+
+HIDDEN_BOARD = [{''} * 8 for x 8 in range(8)]
+GUESS_BOARD = [{''} * 8 for x 8 in range(8)]
+
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+
+def board ():
+    pass
+
+def get_ship 
