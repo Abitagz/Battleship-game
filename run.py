5d08527d4bd82b092bf12f83355106424c61f1bf
Abitagz <helloabita2005@yahoo.com>
2021-09-18 22:07:28 +0000

    Add a for loop to the print board function

--- run.py
+++ run.py
@@ -9,8 +9,14 @@ GUESS_BOARD = [{''} * 8 for x in range(8)]
 letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
 
-def board():
-    pass
+def print_board(board):
+    print('  A B C D E F G H')
+    print('  ---------------')
+    row_namuber = 1
+    for row in board:
+        print for row in board:
+          print("%d|%s|" % (row_number, "|".join(row)))
+          row_number += 1
 
 
 def create_ships():
@@ -19,6 +25,11 @@ def create_ships():
 
 def get_ship_location():
     pass
+
+
+create_ships()
+turns = 10
+#while turns > 0:
 
 
 def count_hit_ships():
