e725b7a28d633c12864806d4ee4c83efd3ca006c
Abitagz <helloabita2005@yahoo.com>
2021-09-18 22:43:19 +0000

    Create 5 ships to board by creating a while for loop

--- run.py
+++ run.py
@@ -1,14 +1,15 @@
+# legend
+# X for palcing ship and hit battleship
+# ' ' for available space
+# '-' for missed shot
 
-
-
+from random import randint
 
 
 HIDDEN_BOARD = [{''} * 8 for x in range(8)]
 GUESS_BOARD = [{''} * 8 for x in range(8)]
 
-letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
-
-
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 
 def print_board(board):
     print('  A B C D E F G H')
     print('  ---------------')
@@ -19,8 +20,12 @@ def print_board(board):
           row_number += 1
 
 
-def create_ships():
-    pass
+def create_ships(board):
+    for ship in range(5):
+        ship_row, ship_column = randint(0,7), randint(0,7)
+        while board[ship_row][ship_column] == 'x':
+            ship_row, ship_column = randint(0,7) randint(0,7)
+        board[ship_row][ship_column] = 'x'
 
 
 def get_ship_location():
